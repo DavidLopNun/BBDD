df9367d058f7cbae4e4fb283de7f0612d1c33c2b
dlopnun1503 <dlopnun1503@g.educaand.es>
2024-03-11 09:48:18 +0100

    Unidades BD

---
+++ Unidad 7/Encriptacion.sql
@@ -0,0 +1,7 @@
+-- Encripta la contrasenia dada
+select password('123456');
+select password('juan_01');
+select password('maria_03');
+select password('miguel_04');
+select password('rocio_05');
+select password('roberto_79');
